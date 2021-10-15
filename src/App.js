c31a17a28898cd9d712c83250dab45e826f75cea
Felipe Silveira <felipelarson@msn.com>
2021-10-15 23:39:19 +0100

    feat: create component Card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-function App() {
-  return (
-    <div >
+import { Card, TextCard } from "./components/Card";
 
-    </div>
+function App() {
+
+  const handleClick = () => {
+    alert("Clickou")
+  }
+
+  return (
+    <>
+      <Card background={"#ccc"} backgroundHover={"coral"} onClick={handleClick}>
+        <TextCard>Name grupo</TextCard>
+        <TextCard>Descrição grupo</TextCard>
+        <TextCard>Categoria grupo</TextCard>
+      </Card>
+      <Card background={"#ccc"} height={200} width={250} backgroundHover={"coral"}>
+        <TextCard>"title": "Calistenia a tarde (15 minutos)"</TextCard>
+        <TextCard>"category": "Sáude"</TextCard>
+        <TextCard>"difficulty": "Muito díficil"</TextCard>
+        <TextCard>"frequency": "Diária"</TextCard>
+        <TextCard>"achieved": false</TextCard>
+        <TextCard>"how_much_achieved": 30</TextCard>
+      </Card>
+    </>
   );
 }
 
---
+++ src/components/Card/index.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+
+
+/**
+ * Exemplo de uso:
+ * 
+ * \<Card width={200} height={200} background={"red"} backgroundHover={"black"}\>
+ * 
+ * valor default: 
+ * 
+ * width = 400px
+ * 
+ * height = 90px
+ * 
+ * background = white
+ * 
+ * backgroundHover = #ccc
+ */
+export const Card = styled.div`
+    background: ${props => props.background ? `${props.background}` : "fff"};
+    width: ${props => props.width ? `${props.width}px` : "400px"};
+    height: ${props => props.height ? `${props.height}px` : "90px"};
+    padding: 1rem;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 12px;
+    cursor: pointer;
+
+    &:hover{
+        background: ${props => props.backgroundHover ? `${props.backgroundHover}` : "#ccc"}
+    }
+`
+
+/**
+ * Exemplo de uso:
+ * 
+ * \<TextCard fontSize={14}\>Texto\</TextCard\>
+ * 
+ * valor default:
+ * 
+ * font-size = 12px
+ * 
+ * color = black
+ */
+export const TextCard = styled.p`
+    margin: 5px;
+    font-size: ${props => props.fontSize ? `${props.fontSize}px` : "12px"};
+    color: ${props => props.color ? `${props.color}` : "black"};
+`
