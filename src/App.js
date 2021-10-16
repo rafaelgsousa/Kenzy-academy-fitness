9e6f960b859758f2e88e0bf8e12f1b68a69a6454
Felipe Silveira <felipelarson@msn.com>
2021-10-16 18:29:51 +0100

    refactor: delete components test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import { ButtonX } from "./components/ButtonX";
-import { FiX } from "react-icons/fi";
-
 function App() {
   return (
     <>
-      <ButtonX>
-        <FiX />
-      </ButtonX>
+
     </>
   );
 }
