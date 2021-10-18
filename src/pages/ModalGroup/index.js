3834e5584d3a15527a32f53a9ae9b578a04abb10
Felipe Silveira <felipelarson@msn.com>
2021-10-18 18:13:25 +0100

    style: position components

--- src/pages/ModalGroup/index.js
+++ src/pages/ModalGroup/index.js
@@ -2,6 +2,7 @@ import Container from './../../components/Container'
 import { Content, GroupButton } from './styles'
 import { Card, TextCard } from './../../components/Card'
 import { Button } from './../../components/Button'
+import { ButtonX } from './../../components/ButtonX'
 
 const ModalGroup = () => {
     return (
@@ -11,16 +12,17 @@ const ModalGroup = () => {
                     <h3>Atividades</h3>
                 </Container>
                 <Container width={"670px"} height={"700px"} opacity={"0.7"}>
+                    <ButtonX width={"50px"} height={"50px"} />
                     <h2>Nome do Grupo</h2>
                     <h3>Categoria</h3>
                     <Card width={"550px"} height={"300px"}>
                         <TextCard>Descrição do Grupo</TextCard>
                     </Card>
                     <GroupButton>
-                        <Button>Atividades</Button>
-                        <Button>Objetivos</Button>
+                        <Button width={"250px"} height={"50px"}>Atividades</Button>
+                        <Button width={"250px"} height={"50px"}>Objetivos</Button>
                     </GroupButton>
-                    <Button>Editar Grupo</Button>
+                    <Button width={"250px"} height={"50px"}>Editar Grupo</Button>
                 </Container>
                 <Container width={"440px"} height={"700px"} opacity={"0.7"}>
                     <h3>Objetivos</h3>
--- src/pages/ModalGroup/styles.js
+++ src/pages/ModalGroup/styles.js
@@ -11,6 +11,7 @@ export const Content = styled.div`
     font-family: 'Roboto';
     letter-spacing: 1.2px;
     font-weight: 300;
+    z-index: 1;
 
     > div{
         margin: 0 1rem;
@@ -18,11 +19,19 @@ export const Content = styled.div`
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        position: relative;
 
         >div{
             display: flex;
             justify-content: center;
             align-items: center;
+        }
+
+        > button:first-child{
+            position: absolute;
+            top: 5px;
+            right: 5px;
+            z-index: 2;
         }
     }
 `
