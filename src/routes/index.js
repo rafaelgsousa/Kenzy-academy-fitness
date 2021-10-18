d799e3295ee2caa8ed167b6a4211d0da217f035e
Kaueh <kaueh1234@gmail.com>
2021-10-18 19:25:57 -0300

    feature: Edit User Modal Schema

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
 import { Switch, Route } from "react-router";
 
 function Routes() {
-  <Switch>
-    <Route exact path="/"></Route>
-    <Route path="/signUp"></Route>
-    <Route path="/login"></Route>
-    <Route path="/dashboard"></Route>
-    <Route path="/habits"></Route>
-    <Route path="/groups"></Route>
-  </Switch>;
+  return (
+    <Switch>
+      <Route exact path="/"></Route>
+      <Route path="/signUp"></Route>
+      <Route path="/login"></Route>
+      <Route path="/dashboard"></Route>
+      <Route path="/habits"></Route>
+      <Route path="/groups"></Route>
+    </Switch>
+  );
 }
 
 export default Routes;
