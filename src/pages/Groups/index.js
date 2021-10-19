a82ad84de65dc91f8d9c38d3d0d458c959d081dd
Felipe Silveira <felipelarson@msn.com>
2021-10-19 19:21:41 +0100

    refactor: ccreated trusthy

--- src/pages/Groups/index.js
+++ src/pages/Groups/index.js
@@ -1,29 +1,29 @@
-import { useContext, useEffect,useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Container from "../../components/Container";
-import {Input} from "../../components/Input/index";
+import { Input } from "../../components/Input/index";
 import { Button } from "../../components/Button";
 import { Card } from "../../components/Card";
-import { GroupsContext} from "../../providers/Groups";
-import {Main} from "./style"
+import { GroupsContext } from "../../providers/Groups";
+import { Main } from "./style"
 import { ButtonX } from "../../components/ButtonX";
 
 export const Groups = () => {
 
-    const [category,setCategory] = useState("")
+    const [category, setCategory] = useState("")
 
-    const {access,subscriptions,groupsOfCategory,getSubscription,
-        getGroupsForCategory,deleteGroup} = useContext(GroupsContext)
-    
-    
+    const { access, subscriptions, groupsOfCategory, getSubscription,
+        getGroupsForCategory, deleteGroup } = useContext(GroupsContext)
 
-    useEffect(()=>{
+
+
+    useEffect(() => {
         getSubscription()
-    },[])
+    }, [])
 
-    useEffect(()=>{
-        console.log("category:",category)
+    useEffect(() => {
+        console.log("category:", category)
         getGroupsForCategory(category)
-    },[category])
+    }, [category])
 
 
     return (
@@ -32,20 +32,22 @@ export const Groups = () => {
                 <Container height="400px" width="300px" opacity="0.9">
                     <Card height="30px" width="80%">My Groups</Card>
                     {console.log(subscriptions)}
-                    {subscriptions.data.map((group,index)=>
-                            <Card key={index} width="150px" height="40px">
-                                <p>{group.name}<ButtonX onClick={()=>deleteGroup(group.id,access)}>X</ButtonX></p>
-                            </Card>
-                        )}
+                    {!!subscriptions.data && subscriptions.data.map((group, index) =>
+                        <Card key={index} width="150px" height="40px">
+                            <p>{group.name}<ButtonX onClick={() => deleteGroup(group.id, access)}>X</ButtonX></p>
+                        </Card>
+                    )}
                     <Button>Create group</Button>
                 </Container>
                 <Container height="400px" width="600px" opacity="0.9">
                     <Card height="30px" width="80%" >Procurar Grupos</Card>
-                    <Input placeholder="search by category" value={category} onChange={(e)=>setCategory(e.target.value)} width="80%"/>
-                    <ul style={{display:"flex",flexWrap:"wrap",
-                    justifyContent:"space-around",alignItems:"center",padding:"5px 30px"}}>
-                        {console.log("categoria:",groupsOfCategory)}
-                        {groupsOfCategory.data.results.map((groups,index)=>
+                    <Input placeholder="search by category" value={category} onChange={(e) => setCategory(e.target.value)} width="80%" />
+                    <ul style={{
+                        display: "flex", flexWrap: "wrap",
+                        justifyContent: "space-around", alignItems: "center", padding: "5px 30px"
+                    }}>
+                        {console.log("categoria:", groupsOfCategory)}
+                        {!!groupsOfCategory.data.results && groupsOfCategory.data.results.map((groups, index) =>
                             <Card key={index} width="150px" height="40px">
                                 <p>{groups.name}</p>
                             </Card>
