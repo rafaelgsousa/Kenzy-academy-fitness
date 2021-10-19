cb094f2a889d14c6535293e5a36591638ea15724
Felipe Silveira <felipelarson@msn.com>
2021-10-19 19:24:04 +0100

    style: created style group

--- src/pages/Groups/index.js
+++ src/pages/Groups/index.js
@@ -57,5 +57,69 @@ export const Groups = () => {
             </Main>
             <Card width="100%">Copyright 2021 Todos os diretos reservados.</Card>
         </>
+
+        // <Box>
+        //     <Blur />
+        //     <Container width={"600px"} height={"760px"} opacity={"0.7"}>
+        //         <h2>Meu Grupos</h2>
+        //         <Content>
+        //             <Card>
+        //                 <ButtonX><FiX /></ButtonX>
+        //                 <TextCard>Grupo novo 1</TextCard>
+        //                 <TextCard>Descrição bolada</TextCard>
+        //                 <TextCard>Saúde</TextCard>
+        //             </Card>
+        //             <Card>
+        //                 <ButtonX><FiX /></ButtonX>
+        //                 <TextCard>Grupo novo 2</TextCard>
+        //                 <TextCard>Descrição bolada</TextCard>
+        //                 <TextCard>Saúde</TextCard>
+        //             </Card>
+        //             <Card>
+        //                 <ButtonX><FiX /></ButtonX>
+        //                 <TextCard>Grupo novo 3</TextCard>
+        //                 <TextCard>Descrição bolada</TextCard>
+        //                 <TextCard>Saúde</TextCard>
+        //             </Card>
+        //             <Card>
+        //                 <ButtonX><FiX /></ButtonX>
+        //                 <TextCard>Grupo novo 4</TextCard>
+        //                 <TextCard>Descrição bolada</TextCard>
+        //                 <TextCard>Saúde</TextCard>
+        //             </Card>
+        //             <Button width={"340px"} height={"95px"}>Criar Grupo</Button>
+        //         </Content>
+        //     </Container>
+        //     <Container width={"1140px"} height={"760px"} opacity={"0.7"}>
+        //         <h2>Procurar Grupos</h2>
+        //         <Input placeholder="Pesquisar por Categoria" />
+        //         <Content>
+        //             <Card>
+        //                 <ButtonX><FiX /></ButtonX>
+        //                 <TextCard>Grupo novo 1</TextCard>
+        //                 <TextCard>Descrição bolada</TextCard>
+        //                 <TextCard>Saúde</TextCard>
+        //             </Card>
+        //             <Card>
+        //                 <ButtonX><FiX /></ButtonX>
+        //                 <TextCard>Grupo novo 2</TextCard>
+        //                 <TextCard>Descrição bolada</TextCard>
+        //                 <TextCard>Saúde</TextCard>
+        //             </Card>
+        //             <Card>
+        //                 <ButtonX><FiX /></ButtonX>
+        //                 <TextCard>Grupo novo 3</TextCard>
+        //                 <TextCard>Descrição bolada</TextCard>
+        //                 <TextCard>Saúde</TextCard>
+        //             </Card>
+        //             <Card>
+        //                 <ButtonX><FiX /></ButtonX>
+        //                 <TextCard>Grupo novo 4</TextCard>
+        //                 <TextCard>Descrição bolada</TextCard>
+        //                 <TextCard>Saúde</TextCard>
+        //             </Card>
+        //         </Content>
+        //     </Container>
+        // </Box>
     )
 }
