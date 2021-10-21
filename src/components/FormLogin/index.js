32df54209fea669514489976def3ddf23eafa14c
Ian Kuntz <ianbertoluchi99@gmail.com>
2021-10-21 16:08:10 -0300

    feature:style_Home_SignUp_Login

--- src/components/FormLogin/index.js
+++ src/components/FormLogin/index.js
@@ -3,6 +3,9 @@ import {useState} from "react"
 import axios from "axios"
 import toast from "react-hot-toast"
 import jwt_decode from "jwt-decode"
+import { Button } from "../Button"
+import logokenzie from "../../assets/img/logokenzie.png"
+import { LoginCss } from "../LoginCss"
 
 const FormLogin = () => {
 
@@ -32,15 +35,66 @@ const FormLogin = () => {
             history.push("/dashboard")
         })
         .catch((err) => toast.error("Login Fail"))
+    }   
+
+    const goTodash = () => {
+        
+        const token = localStorage.getItem("@KAF_userToken");
+            
+        if (token) {
+
+        history.push("/dashboard");
+
+    }else{
+        toast.error("Faça login para acessar")
+    }
+    }
+    const logout = () => {
+        localStorage.clear()
+        history.push("/")
+        toast.success("Logout feito com sucesso")
     }
 
     return(
         <div>
-            <form onSubmit={(evt) => SubmitLogin(evt)}>
+            <LoginCss>
+            <header>
+                    <div className="logo">
+                        <img src={logokenzie} alt="logokenzie"/>
+                        <p>Kenzie Academy</p>
+                    </div>
+                    
+                    <div className="nav">
+                    <div className="training">
+                        <p>#Training <br/> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Like a Dev</p>
+                    </div>
+                        <nav onClick={logout} className="signup">Logout</nav>
+                        <nav onClick={() => history.push("/")} className="signup">Home</nav>
+                        <nav onClick={() => history.push("/signUp")} className="signup">Sign Up</nav>
+                    </div>
+                <div className="buttons">
+                    <Button>Quem Somos</Button>
+                    <Button>Planos</Button>
+                    <Button onClick={goTodash}>Dashboard</Button>
+                </div>
+                </header>
+                    <h1 className="h1login">LOGIN</h1>
+                <div className="container">
+            <form onSubmit={(evt) => SubmitLogin(evt)} className="form">
                 <input placeholder="Username" onChange={(evt) => setUsername(evt.target.value)} value={username}/>
                 <input placeholder="Password" type="password" onChange={(evt) => setPassword(evt.target.value)} value={password}/>
-                <button type="submit">Login</button>
+                <Button type="submit" className="Login">Login</Button>
             </form>
+            </div>
+            <footer>
+                    <div className="infoFooter">
+                        <p>R. Gen. Mário Tourinho, 1733 <br/> Curitiba - PR, 80740-000</p>
+                        <p>Segunda à Sexta <br/> 7:30 às 20:30</p>
+                        <p>Contato <br/> (41)99745-3268</p>
+                    </div>
+                    <p className="copyright">© Copyright 2021. Todos os direitos reservados. Kenzie Estrutura Fitness.</p>
+                </footer>
+            </LoginCss>
         </div>
     )
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,84 @@
 import {useHistory} from "react-router"
 import { HomeCss } from "../HomeCss"
 import { Button } from "../Button"
+import logokenzie from "../../assets/img/logokenzie.png"
+import home1 from "../../assets/img/home1.jpeg"
+import home2 from "../../assets/img/home2.jpeg"
+import home3 from "../../assets/img/home3.jpeg"
+import home4 from "../../assets/img/home4.jpeg"
+import toast from "react-hot-toast"
+
 
 const Home = () => {
 
     const history = useHistory()
 
+    const goTodash = () => {
+        
+        const token = localStorage.getItem("@KAF_userToken");
+            
+        if (token) {
+
+        history.push("/dashboard");
+
+    }else{
+        toast.error("Faça login para acessar")
+    }
+    }
+
+    const logout = () => {
+        localStorage.clear()
+        history.push("/")
+        toast.success("Logout feito com sucesso")
+    }
+
     return(
         <div>
             <HomeCss>
                 <header>
+                    <div className="logo">
+                        <img src={logokenzie} alt="logokenzie"/>
+                        <p>Kenzie Academy</p>
+                    </div>
+                    
                     <div className="nav">
+                    <div className="training">
+                        <p>#Training <br/> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Like a Dev</p>
+                    </div>
+                    <nav onClick={logout} className="signup">Logout</nav>
                         <nav onClick={() => history.push("/login")} className="login">Login</nav>
                         <nav onClick={() => history.push("/signUp")} className="signup">Sign Up</nav>
                     </div>
+                <div className="buttons">
                     <Button>Quem Somos</Button>
                     <Button>Planos</Button>
+                    <Button onClick={goTodash}>Dashboard</Button>
+                </div>
                 </header>
+                    <div className="container">
+                        <div className="images">
+                            <span>
+                                <img src={home1} alt="home1" width={"240px"} height={"220px"}/>
+                            </span>
+                            <span>
+                                <img src={home2} alt="home2" width={"240px"} height={"220px"}/>
+                            </span>
+                            <span>
+                                <img src={home3} alt="home3" width={"240px"} height={"220px"}/>
+                            </span>
+                            <span>
+                                <img src={home4} alt="home4" width={"240px"} height={"220px"}/>
+                            </span>
+                        </div>
+                    </div>
+                    
                 <footer>
-                    <p>© Copyright 2021. Todos os direitos reservados. Kenzie Estrutura Fitness.</p>
+                    <div className="infoFooter">
+                        <p>R. Gen. Mário Tourinho, 1733 <br/> Curitiba - PR, 80740-000</p>
+                        <p>Segunda à Sexta <br/> 7:30 às 20:30</p>
+                        <p>Contato <br/> (41)99745-3268</p>
+                    </div>
+                    <p className="copyright">© Copyright 2021. Todos os direitos reservados. Kenzie Estrutura Fitness.</p>
                 </footer>
             </HomeCss>
             
